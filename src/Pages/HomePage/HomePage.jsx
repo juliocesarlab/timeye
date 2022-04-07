5aa22efdf05ca6cf99558728186bab0937013edc
unknown <juliostdfac@gmail.com>
2022-04-06 23:05:39 -0300

    clear logs

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,27 +1,30 @@
-
-import { NavLink } from "react-router-dom";
-import logoImage from "../../assets/Timeye-only-logo.png";
-import textLogo from "../../assets/Timeye-only-text.png";
-import { Navbar } from "../../Components/Navbar/Navbar";
-import { Writer } from "../../Components/Writer/Writer";
+import { NavLink } from 'react-router-dom';
+import logoImage from '../../assets/Timeye-only-logo.png';
+import textLogo from '../../assets/Timeye-only-text.png';
+import { Navbar } from '../../Components/Navbar/Navbar';
+import { Writer } from '../../Components/Writer/Writer';
 import { useAuth } from '../../Hooks/useAuth';
 import { HomeView } from './styles.js';
 
 export function HomePage() {
-
-  const data = useAuth()
-  console.log(data)
+  const data = useAuth();
 
   return (
     <>
-      <Navbar options={[{name: "What is Timeye ?", goTo: "/about"}, {name: "Login", goTo: "/login"}]}/>
+      <Navbar
+        options={[
+          { name: 'What is Timeye ?', goTo: '/about' },
+          { name: 'Login', goTo: '/login' },
+        ]}
+      />
       <div className="default-container">
         <HomeView>
           <div className="apresentation">
             <img src={textLogo} alt="Timeye" />
-            <h1>Don't waste your 
+            <h1>
+              Don't waste your
               <span>
-                <Writer phrasesArr={["time.", "money."]}/>
+                <Writer phrasesArr={['time.', 'money.']} />
               </span>
             </h1>
             <NavLink to={'/register'}>
@@ -34,6 +37,5 @@ export function HomePage() {
         </HomeView>
       </div>
     </>
-  )
+  );
 }
-
